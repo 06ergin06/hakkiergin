5e1da331c974ea1fa08ba8eb5d89e75be0e52ed7
06ergin06 <ibrahimh.ergin@gmail.com>
2025-02-17 22:40:15 +0300

    python added

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -3,7 +3,7 @@ import {
   FaCss3,
   FaJs,
   FaJava,
-  FaLinux,
+  FaPython,
   FaFlutter,
 } from "react-icons/fa6";
 import { RiNextjsFill } from "react-icons/ri";
@@ -14,8 +14,9 @@ export default function Icons() {
       <FaCss3 className="w-8 h-8 dark:text-[#2C5EE4] text-[#2C5EE4] hover:opacity-80 transition-all" />
       <FaJs className="w-8 h-8 dark:text-[#F6E03B] text-yellow-500 hover:opacity-80 transition-all" />
       <FaJava className="w-8 h-8 dark:text-[#EE4534] text-[#EE4534] hover:opacity-80 transition-all" />
+      <FaPython className="w-8 h-8 dark:text-[#FECE40] text-[#366E9E] hover:opacity-80 transition-all" />
       <RiNextjsFill className="w-8 h-8 dark:text-[#727272] hover:opacity-80 transition-all" />
-      <FaLinux className="w-8 h-8 dark:text-white hover:opacity-80 transition-all" />
+      {/* <FaLinux className="w-8 h-8 dark:text-white hover:opacity-80 transition-all" /> */}
       <FaFlutter className="w-8 h-8 dark:text-[#45D2FB] text-[#45D2FB] hover:opacity-80 transition-all" />
     </div>
   );
