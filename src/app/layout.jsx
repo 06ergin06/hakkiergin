eabf3c14f3d47be9e990a8698ae787bc0bf5f8e5
06ergin06 <ibrahimh.ergin@gmail.com>
2025-04-26 21:29:32 +0300

    update metadata description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,8 +4,15 @@ import { Montserrat } from "next/font/google";
 export const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata = {
   title: "İbrahim Hakkı Ergin",
-  description: "Who's İbrahim Hakkı Ergin",
-  keywords: ["webtas", "İbrahim Hakkı", "İbrahim Hakkı Ergin", "software engineering", "front-end development"],
+  description:
+    "İbrahim Hakkı Ergin, a software engineering student who loves programming, front-end development, and open source technologies.",
+  keywords: [
+    "webtas",
+    "İbrahim Hakkı",
+    "İbrahim Hakkı Ergin",
+    "software engineering",
+    "front-end development",
+  ],
   authors: [{ name: "İbrahim Hakkı Ergin" }],
   creator: "İbrahim Hakkı Ergin",
 };
@@ -16,7 +23,13 @@ export default function RootLayout({ children }) {
       <body className={`${montserrat.className} antialiased`}>
         {children}
         <footer className="center text-[--foreground] underline">
-          <a href="https://github.com/06ergin06/hakkiergin" target="_blank" rel="noopener noreferrer">The source code of this website</a>
+          <a
+            href="https://github.com/06ergin06/hakkiergin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            The source code of this website
+          </a>
         </footer>
       </body>
     </html>
