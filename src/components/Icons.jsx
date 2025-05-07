820959cf7be4262e9fb023d8e1a668379423f75d
06ergin06 <ibrahimh.ergin@gmail.com>
2025-05-07 12:23:58 +0300

    vercel and meta boycott

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -4,17 +4,16 @@ import {
   FaJs,
   FaJava,
   FaPython,
-  FaReact,
+  FaVuejs
 } from "react-icons/fa6";
-import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
+import {RiTailwindCssFill } from "react-icons/ri";
 export default function Icons() {
   return (
     <div className="center gap-3 flex-wrap">
       <FaHtml5 className="w-8 h-8 dark:text-[#E34C26] text-[#E34C26] hover:opacity-80 transition-all" title="HTML5" aria-label="HTML5" />
       <FaCss3 className="w-8 h-8 dark:text-[#2C5EE4] text-[#2C5EE4] hover:opacity-80 transition-all" title="CSS3" aria-label="CSS3" />
       <FaJs className="w-8 h-8 dark:text-[#F6E03B] text-yellow-500 hover:opacity-80 transition-all" title="JavaScript" aria-label="JavaScript" />
-      <FaReact className="w-8 h-8 dark:text-[#58C4DC] text-[#58C4DC] hover:opacity-80 transition-all" title="React" aria-label="React" />
-      <RiNextjsFill className="w-8 h-8 dark:text-[#727272] hover:opacity-80 transition-all" title="Next.js" aria-label="Next.js" />
+      <FaVuejs className="w-8 h-8 dark:text-[#41B883] text-[#41B883] hover:opacity-80 transition-all" title="Vue.js" aria-label="Vue.js" />
       <RiTailwindCssFill className="w-8 h-8 dark:text-[#28BBFB] text-[#28BBFB] hover:opacity-80 transition-all" title="TailwindCss" aria-label="TailwindCss" />
       <FaJava className="w-8 h-8 dark:text-[#EE4534] text-[#EE4534] hover:opacity-80 transition-all" title="Java" aria-label="Java" />
       <FaPython className="w-8 h-8 dark:text-[#FECE40] text-[#366E9E] hover:opacity-80 transition-all" title="Python" aria-label="Python" />
